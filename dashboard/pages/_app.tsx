eaecfdd8e2d1ef152a745df2658c9e13776df1af
James Wallis <j@wallis.dev>
2021-04-06 13:57:04 +0100

    Remove getInitialProps from app.tsx
    Signed-off-by: James Wallis <j@wallis.dev>

--- dashboard/pages/_app.tsx
+++ dashboard/pages/_app.tsx
@@ -1,26 +1,8 @@
-import App, { AppContext, AppProps } from 'next/app'
-import IAzureArticleData from '../../common/interfaces/IAzureArticleData'
-import IAzureFollowerData from '../../common/interfaces/IAzureFollowerData'
-import IUser from '../interfaces/IUser'
-import { getAzureArticleData, getAzureFollowerData } from '../lib/azure'
-import { getUser } from '../lib/devto'
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return <Component {...pageProps} />
-}
-
-MyApp.getInitialProps = async (context: AppContext) => {
-    const appProps = await App.getInitialProps(context)
-    const promises: Promise<IAzureArticleData | IAzureFollowerData | IUser>[] = [
-        getAzureArticleData(),
-        getAzureFollowerData(),
-        getUser(),
-    ]
-
-    const [azureArticleData, azureFollowerData, user] = await Promise.all(promises)
-
-    return { pageProps: { ...appProps.pageProps, azureArticleData, azureFollowerData, user } }
 }
 
 export default MyApp

15a328b247ec1e2dad88926b670431b3c60b240a
James Wallis <j@wallis.dev>
2021-04-13 23:47:05 +0100

    Support over 1000 followers
    Signed-off-by: James Wallis <j@wallis.dev>

--- azure-functions/lib/utils/devto.ts
+++ azure-functions/lib/utils/devto.ts
@@ -3,9 +3,10 @@ import IDevToArticle from '../../interfaces/IDevToArticle'
 import IDevToFollower from '../../interfaces/IDevToFollower'
 
 const token: string = process.env['DEVTO_TOKEN']
+const perPage = 1000
 
 export const getArticles = async (): Promise<IDevToArticle[]> => {
-    const params = { per_page: 1000 }
+    const params = { per_page: perPage }
     const headers = { 'api-key': token }
     const { data }: AxiosResponse = await axios.get(`https://dev.to/api/articles/me/all`, {
         params,
@@ -14,12 +15,26 @@ export const getArticles = async (): Promise<IDevToArticle[]> => {
     return data
 }
 
-export const getFollowers = async (): Promise<IDevToFollower[]> => {
-    const params = { per_page: 1000 }
+const getPageOfFollowers = async (page: number): Promise<IDevToFollower[]> => {
+    const params = { per_page: perPage, page }
     const headers = { 'api-key': token }
     const { data }: AxiosResponse = await axios.get(`https://dev.to/api/followers/users`, {
         params,
         headers,
     })
     return data
+}
+
+export const getFollowers = async (): Promise<IDevToFollower[]> => {
+    let numReturned = perPage
+    let page = 1
+    const totalFollowers = []
+    while (numReturned === perPage) {
+        const followers = await getPageOfFollowers(page)
+        totalFollowers.push(...followers)
+
+        numReturned = followers.length
+        page++
+    }
+    return totalFollowers
 }
